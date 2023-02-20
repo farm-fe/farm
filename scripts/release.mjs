3c984938e49220cb307bbc5970e42076dfbb33b3
brightwu <1521488775@qq.com>
2023-02-21 01:02:59 +0800

    chore: bump version (#33)

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -4,3 +4,16 @@ import { execSync } from "child_process";
 execSync("npm run build", { stdio: "inherit" });
 // publish node packages
 execSync("npx changeset publish", { stdio: "inherit" });
+
+// publish all packages under packages/core/npm
+// TODO: investigate why napi prepublish doesn't work
+const packages = [
+  'darwin-x64',
+  'darwin-arm64',
+  'linux-x64-gnu',
+  'win32-x64-msvc',
+];
+
+packages.forEach((pkg) => {
+  execSync(`npm publish packages/core/npm/${pkg}`, { stdio: "inherit" });
+});
