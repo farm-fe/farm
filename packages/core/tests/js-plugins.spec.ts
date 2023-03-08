12445719526647608232f05255be31b4665847c4
Bright Wu <1521488775@qq.com>
2023-03-08 22:20:31 +0800

    fix: js plugins test error

--- packages/core/tests/js-plugins.spec.ts
+++ packages/core/tests/js-plugins.spec.ts
@@ -66,7 +66,7 @@ test('Js Plugin Execution - resolve', async () => {
   ]);
 
   await compiler.compile();
-  compiler.writeResourcesToDisk();
+  await compiler.writeResourcesToDisk();
   const outputFilePath = getOutputFilePath('resolve');
 
   if (process.platform === 'win32') {
@@ -100,7 +100,7 @@ test('Js Plugin Execution - load', async () => {
   ]);
 
   await compiler.compile();
-  compiler.writeResourcesToDisk();
+  await compiler.writeResourcesToDisk();
   const outputFilePath = getOutputFilePath('load');
 
   if (process.platform === 'win32') {
@@ -133,7 +133,7 @@ test('Js Plugin Execution - transform', async () => {
   ]);
 
   await compiler.compile();
-  compiler.writeResourcesToDisk();
+  await compiler.writeResourcesToDisk();
   const outputFilePath = getOutputFilePath('transform');
 
   if (process.platform === 'win32') {
