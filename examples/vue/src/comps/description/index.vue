36fcdcafb916f3896f0a1aba2b07585f86bafca8
余尚运 <yushangyun@yushangyundeMacBook-Air.local>
2023-03-15 01:39:20 +0800

    update template of vue

--- examples/vue/src/comps/description/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div className='description'>
-    <p>Farm is a supper fast building engine written in rust. 🔥 </p>
-    <p>Visit https://github.com/farm-fe/farm for details</p>
-  </div>
-</template>
-
--- examples/vue/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-
-import Main from "./main.vue";
-const container = document.querySelector("#root")!;
-createApp(Main).mount(container);
