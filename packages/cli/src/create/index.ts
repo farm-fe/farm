e14dd16c61cea408ee80a1b8715424b2480eebde
余尚运 <yushangyun@yushangyundeMacBook-Air.local>
2023-03-19 19:35:25 +0800

    update cli

--- packages/cli/src/create/index.ts
+++ packages/cli/src/create/index.ts
@@ -1,15 +1,50 @@
 import path from 'node:path';
 import { copyFiles, TEMPLATES_DIR } from '../utils.js';
-
+import inquirer from 'inquirer';
 import chalk from 'chalk';
 
 const TEMPLATE_REACT = path.join(TEMPLATES_DIR, 'react');
+// const TEMPLATE_VUE = path.join(TEMPLATES_DIR, 'vue');
+const REACT = 'React';
+const VUE = 'Vue';
 
 export async function create(): Promise<void> {
-  const dest = path.join(process.cwd(), 'farm-react');
+  const { name: projectName } = await inquirer.prompt({
+    type: 'input',
+    name: 'name',
+    message: 'please input project name',
+    default: 'farm-project',
+  });
 
-  copyFiles(TEMPLATE_REACT, dest);
+  const { framework } = await inquirer.prompt({
+    type: 'list',
+    name: 'framework',
+    message: 'please choose a framework',
+    choices: [
+      {
+        name: chalk.red('1) React'),
+        value: REACT,
+      },
+      {
+        name: chalk.green('2) Vue'),
+        value: VUE,
+      },
+    ],
+  });
+  const dest = path.join(process.cwd(), projectName);
+  if (framework === REACT) {
+    copyFiles(TEMPLATE_REACT, dest);
+    logger(dest, projectName);
+  } else if (framework === VUE) {
+    //TODO copy vue template
+    //copyFiles(TEMPLATE_VUE, dest);
+    //logger(dest, projectName);
+  } else {
+    throw new Error(`Please choose legal template!`);
+  }
+}
 
+function logger(dest: string, projectName: string) {
   console.log(
     chalk.green('Created a new Farm app in ') +
       chalk.bold(dest) +
@@ -17,7 +52,7 @@ export async function create(): Promise<void> {
   );
   console.log(
     `Run ${chalk.cyan.bold(
-      'cd farm-react && npm i && npm start'
+      `cd ${projectName} && npm i && npm start`
     )} to get started.`
   );
 }
