ebde7b8d3868dd724c192c4508923641248b1b73
余尚运 <yushangyun@yushangyundeMacBook-Air.local>
2023-03-20 23:05:04 +0800

    add template of vue

---
+++ examples/vue/src/components/Title.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="farm-title-wrapper">
+        <img src="../../assets/logo.svg" class="farm-logo" alt="Farm Logo" />
+        <h1 class="farm-title">Farm</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+})
+</script>
+
+<style>
+.farm-title {
+    color: rgb(113, 26, 95);
+    font-size: 100px;
+    font-style: italic;
+    margin: 0;
+}
+
+.farm-title-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.farm-logo {
+    width: 150px;
+}
+</style>
