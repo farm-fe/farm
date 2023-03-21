e33c3d5892fda18e7c4deca0a2d91170baa5c422
余尚运 <yushangyun@yushangyundeMacBook-Air.local>
2023-03-21 11:42:10 +0800

    add template of vue in cli/template

---
+++ packages/cli/templates/vue/farm.config.ts
@@ -0,0 +1,20 @@
+// change to @farmfe/core/config when resolve support conditional exports
+import { defineFarmConfig } from '@farmfe/core/dist/config';
+
+export default defineFarmConfig({
+  compilation: {
+    input: {
+      index: './index.html',
+    },
+    resolve: {
+      symlinks: true,
+      mainFields: ['module', 'main', 'customMain'],
+    },
+    output: {
+      path: './build',
+    },
+  },
+  server: {
+    hmr: true,
+  },
+});
