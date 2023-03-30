12e94d0f7efc7ac956ae43c148484906b56f5639
erkelost <1256029807@qq.com>
2023-03-30 09:16:36 +0800

    feat: plugin-resolve-exports field

---
+++ crates/plugin_resolve/tests/exports.rs
@@ -0,0 +1,67 @@
+use farmfe_core::{config::ResolveConfig, plugin::ResolveKind};
+use farmfe_plugin_resolve::resolver::Resolver;
+use farmfe_testing_helpers::fixture;
+
+#[test]
+fn resolve_exports_basic() {
+  fixture!(
+    "tests/fixtures/resolve-node-modules/exports/index.ts",
+    |file, _| {
+      let cwd = file.parent().unwrap().to_path_buf();
+      let resolver = Resolver::new(ResolveConfig::default());
+      // Parsing packages in node_modules
+      let resolved = resolver.resolve("basic", cwd.clone(), &ResolveKind::Import);
+      assert!(resolved.is_some());
+      let resolved = resolved.unwrap();
+      assert_eq!(
+        resolved.resolved_path,
+        cwd
+          .join("node_modules")
+          .join("basic")
+          .join("lib")
+          .join("basic-exports.js")
+          .to_string_lossy()
+          .to_string()
+      );
+    }
+  );
+}
+
+#[test]
+fn resolve_exports_replace() {
+  fixture!(
+    "tests/fixtures/resolve-node-modules/exports/node_modules/replace/package.json",
+    |file, _| {
+      let cwd = file.parent().unwrap().to_path_buf();
+      let resolver = Resolver::new(ResolveConfig::default());
+      let resolved = resolver.resolve(".", cwd.clone(), &ResolveKind::Import);
+      assert!(resolved.is_some());
+      let resolved = resolved.unwrap();
+      assert_eq!(
+        resolved.resolved_path,
+        cwd
+          .join("lib")
+          .join("basic-exports.js")
+          .to_string_lossy()
+          .to_string()
+      );
+
+      let resolved = resolver.resolve("./submodule.js", cwd.clone(), &ResolveKind::Import);
+      assert!(resolved.is_some());
+      let resolved = resolved.unwrap();
+      assert_eq!(
+        resolved.resolved_path,
+        cwd
+          .join("lib")
+          .join("submodule.js")
+          .to_string_lossy()
+          .to_string()
+      );
+    }
+  );
+}
+
+#[test]
+fn resolve_exports_import_require() {
+  // TODO - this should be value with object type
+}
