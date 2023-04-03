8f4bd4c86fac80d292916a64da84e4f304c41a55
erkelost <1256029807@qq.com>
2023-04-03 16:39:57 +0800

    chore: change framework sequence optimize code

--- packages/create-farm/tsup.config.ts
+++ packages/create-farm/tsup.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'tsup';
 export default defineConfig({
   minify: true,
-  bundle: true,
   entry: ['./index.ts'],
   platform: 'node',
   target: 'node16',
   treeshake: true,
+  esbuildPlugins: [
+    {
+      name: 'alias',
+      setup({ onResolve, resolve }) {
+        onResolve(
+          { filter: /^prompts$/, namespace: 'file' },
+          async ({ importer, resolveDir }) => {
+            const result = await resolve('prompts/lib/index.js', {
+              importer,
+              resolveDir,
+              kind: 'import-statement',
+            });
+            return result;
+          }
+        );
+      },
+    },
+  ],
 });
