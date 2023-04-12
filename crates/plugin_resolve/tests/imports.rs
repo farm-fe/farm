b025330b1b30c85b4642213dd657b9771c93cfff
erkelost <1256029807@qq.com>
2023-04-12 21:58:46 +0800

    feat: resolver support import alias deps

--- crates/plugin_resolve/tests/imports.rs
+++ crates/plugin_resolve/tests/imports.rs
@@ -25,6 +25,7 @@ fn resolve_imports_basic() {
     |file, _| {
       let cwd = file.parent().unwrap().to_path_buf();
       let resolver = Resolver::new(ResolveConfig::default());
+
       // Parsing packages in node_modules
       let resolved = resolver.resolve("#ansi-styles", cwd.clone(), &ResolveKind::Import);
       assert!(resolved.is_some());
@@ -39,7 +40,20 @@ fn resolve_imports_basic() {
           .to_string_lossy()
           .to_string()
       );
+    }
+  );
+}
 
+#[test]
+fn resolve_imports_replace_object() {
+  fixture!(
+    // TODO node environment
+    "tests/fixtures/resolve-node-modules/imports/node_modules/chalk/package.json",
+    |file, _| {
+      let cwd = file.parent().unwrap().to_path_buf();
+      let resolver = Resolver::new(ResolveConfig::default());
+
+      // Parsing packages in node_modules
       let resolved = resolver.resolve("#supports-color", cwd.clone(), &ResolveKind::Import);
       assert!(resolved.is_some());
       let resolved = resolved.unwrap();
@@ -50,6 +64,34 @@ fn resolve_imports_basic() {
           .join("vendor")
           .join("supports-color")
           .join("browser.js")
+          .to_string_lossy()
+          .to_string()
+      );
+    }
+  );
+}
+
+#[test]
+fn resolve_imports_replace_deps() {
+  fixture!(
+    // TODO node environment
+    "tests/fixtures/resolve-node-modules/imports/node_modules/chalk/package.json",
+    |file, _| {
+      let cwd = file.parent().unwrap().to_path_buf();
+      let resolver = Resolver::new(ResolveConfig::default());
+
+      // import resolve other deps like `"#ansi-styles-execa": "execa"`
+      let resolved = resolver.resolve("#ansi-styles-execa", cwd.clone(), &ResolveKind::Import);
+      assert!(resolved.is_some());
+      let resolved = resolved.unwrap();
+      assert_eq!(
+        resolved.resolved_path,
+        cwd
+          .parent()
+          .unwrap()
+          .to_path_buf()
+          .join("execa")
+          .join("index.js")
           .to_string_lossy()
           .to_string()
       );
