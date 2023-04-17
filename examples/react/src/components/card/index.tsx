a8f709b4ee4cfb9cdfbb124bcb4eb76d7781a170
erkelost <1256029807@qq.com>
2023-04-17 22:04:35 +0800

    chore: update template

--- examples/react/src/components/card/index.tsx
+++ examples/react/src/components/card/index.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import './index.css';
 
-export function FarmCard(props: PropsWithChildren<{ to: string }>) {
+export function FarmCard(props: PropsWithChildren) {
   return (
     <div className="card">
       <div className="card-content">{props.children}</div>
