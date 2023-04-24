92cdedf0d9dd01a980632ccab704007c8a319105
erkelost <1256029807@qq.com>
2023-04-24 13:37:54 +0800

    chore: remove template

--- packages/cli/templates/react/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Main } from './main';
-
-const container = document.querySelector('#root');
-const root = createRoot(container);
-
-root.render(<Main />);
--- packages/cli/templates/vue/src/Main.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Welcome />
-</template>
-
-<script lang="ts" setup>
-import Welcome from './components/Welcome.vue';
-</script>
