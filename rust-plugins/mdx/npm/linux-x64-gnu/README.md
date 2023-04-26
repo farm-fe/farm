2cddf6e12f4fc655ed34ca865319c8fe3fcfce12
erkelost <1256029807@qq.com>
2023-04-26 10:31:04 +0800

    feat: add rust-plugin/mdx

---
+++ rust-plugins/mdx/npm/linux-x64-gnu/README.md
@@ -0,0 +1,3 @@
+# `farmfe-plugin-mdx-linux-x64-gnu`
+
+This is the **x86_64-unknown-linux-gnu** binary for `farmfe-plugin-mdx`
---
+++ rust-plugins/mdx/src/lib.rs
@@ -0,0 +1,21 @@
+#![deny(clippy::all)]
+
+use farmfe_core::{config::Config, plugin::Plugin};
+
+use farmfe_macro_plugin::farm_plugin;
+
+#[farm_plugin]
+pub struct FarmPluginMdx {}
+
+impl FarmPluginMdx {
+  fn new(config: &Config, options: String) -> Self {
+    
+    Self {}
+  }
+}
+
+impl Plugin for FarmPluginMdx {
+  fn name(&self) -> &str {
+    "FarmPluginMdx"
+  }
+}
