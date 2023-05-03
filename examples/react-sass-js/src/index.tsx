d819bbb2308f01e080385b9d93231ee6737df175
Nirvana <1357711537@qq.com>
2023-05-03 17:55:29 +0800

    feat: add the check of implementation for sass

---
+++ examples/react-sass-js/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Main } from './main';
+import './index.scss';
+
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
+root.render(<Main />);
