e2c3d8e96546bf8f384c21baa01ae8231e90c1d1
erkelost <1256029807@qq.com>
2023-05-17 22:46:59 +0800

    chore: add test resolve error

---
+++ examples/ts/farm.config.ts
@@ -0,0 +1,36 @@
+import { builtinModules } from 'module';
+
+/**
+ * @type {import('@farmfe/core').UserConfig}
+ */
+export default {
+  compilation: {
+    input: {
+      index: './index.ts'
+    },
+    output: {
+      path: 'dist',
+      filename: 'index.[ext]',
+      targetEnv: 'node'
+    },
+    external: [
+      ...builtinModules.map((m) => `^${m}$`),
+      ...builtinModules.map((m) => `^node:${m}$`)
+    ],
+    partialBundling: {
+      moduleBuckets: [
+        {
+          name: 'node.bundle.js',
+          test: ['.+']
+        }
+      ]
+    },
+    minify: false,
+    sourcemap: false,
+    presetEnv: false,
+    treeShaking: false,
+  },
+  server: {
+    hmr: false
+  }
+};
---
+++ examples/ts/index.ts
@@ -0,0 +1,6 @@
+import path from 'node:path';
+import { Project } from 'ts-morph';
+import { obj } from '@relaxed/utils';
+// console.log(Project);
+console.log(path);
+console.log(obj);
