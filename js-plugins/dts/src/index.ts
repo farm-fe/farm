e63ce4bb4929368a7b1b6474244506454af8701d
erkelost <1256029807@qq.com>
2023-05-29 22:57:19 +0800

    chore: update lock

--- js-plugins/dts/src/index.ts
+++ js-plugins/dts/src/index.ts
@@ -1,68 +1,65 @@
-// import { JsPlugin, UserConfig } from '@farmfe/core';
-// import fs from 'fs';
-// import { Project } from 'ts-morph';
-// import { getResolvedOptions, handleExclude, handleInclude } from './utils.js';
-// export default function farmDtsPlugin(
-//   farmDtsPluginOptions: any = {}
-// ): JsPlugin {
-//   // options hooks to get farmConfig
-//   let farmConfig: UserConfig['compilation'];
-//   const resolvedOptions = getResolvedOptions(farmDtsPluginOptions);
-
-//   const exclude = handleExclude(resolvedOptions);
-//   const include = handleInclude(resolvedOptions);
-//   return {
-//     name: 'farm-dts-plugin',
-//     config(config: any) {
-//       farmConfig = config || {};
-//       return config;
-//     },
-//     load: {
-//       filters: {
-//         resolvedPaths: ['.ts$']
-//       },
-//       async executor(params: any, ctx: any) {
-//         const { resolvedPath } = params;
-//         const data = await fs.promises.readFile(resolvedPath, 'utf-8');
-
-//         let source = data;
-//         console.log(source);
-
-//         return {
-//           content: data,
-//           moduleType: 'ts'
-//         };
-//       }
-//     },
-//     transform: {
-//       filters: {
-//         // resolvedPaths: ['.ts$', ...include]
-//         resolvedPaths: ['.ts$']
-//       },
-//       async executor(params: any, ctx: any) {
-//         console.log(params);
-//         const project = new Project();
-//         // const sourceFile = project.createSourceFile(
-//         //   params.resolvedPath,
-//         //   params.content
-//         // );
-//         // console.log(sourceFile);
-
-//         // const dtsFile = sourceFile
-//         //   .emitToMemory()
-//         //   .getFiles()
-//         //   .find((f) => f.filePath.endsWith('.d.ts'))!;
-//         // console.log(dtsFile.text);
-//         let source = '';
-//         return {
-//           content: source,
-//           moduleType: 'ts'
-//         };
-//       }
-//     }
-//   };
-// }
-
+import { JsPlugin, UserConfig } from '@farmfe/core';
+import fs from 'node:fs';
 import { Project } from 'ts-morph';
+import { getResolvedOptions, handleExclude, handleInclude } from './utils.js';
+export default function farmDtsPlugin(
+  farmDtsPluginOptions: any = {}
+): JsPlugin {
+  // options hooks to get farmConfig
+  let farmConfig: UserConfig['compilation'];
+  const resolvedOptions = getResolvedOptions(farmDtsPluginOptions);
 
-console.log(Project);
+  // const exclude = handleExclude(resolvedOptions);
+  // const include = handleInclude(resolvedOptions);
+  return {
+    name: 'farm-dts-plugin',
+    config(config: any) {
+      farmConfig = config || {};
+      return config;
+    },
+    load: {
+      filters: {
+        resolvedPaths: ['.ts$']
+      },
+      async executor(params: any, ctx: any) {
+        const { resolvedPath } = params;
+        const data = await fs.promises.readFile(resolvedPath, 'utf-8');
+
+        let source = data;
+        console.log(source);
+
+        return {
+          content: data,
+          moduleType: 'ts'
+        };
+      }
+    },
+    transform: {
+      filters: {
+        // resolvedPaths: ['.ts$', ...include]
+        resolvedPaths: ['.ts$']
+      },
+      async executor(params: any, ctx: any) {
+        console.log(params);
+        const project = new Project();
+        console.log(project);
+        // const sourceFile = project.createSourceFile(
+        //   params.resolvedPath,
+        //   params.content
+        // );
+        // console.log(sourceFile);
+
+        // const dtsFile = sourceFile
+        //   .emitToMemory()
+        //   .getFiles()
+        //   .find((f) => f.filePath.endsWith('.d.ts'))!;
+        // console.log(dtsFile.text);
+        let source = '';
+        return {
+          content: source,
+          moduleType: 'ts'
+        };
+      }
+    }
+  };
+}
