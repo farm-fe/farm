8f8553b894a32c43adda5107fed956c2c08de642
Bright <1521488775@qq.com>
2023-06-20 08:35:12 +0800

    fix: rust type error

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -222,7 +222,7 @@ impl JsCompiler {
     let context = self.compiler.context();
     let resources = context.resources_map.lock();
 
-    if let Some(node_env) = std::env::var("NODE_ENV") {
+    if let Ok(node_env) = std::env::var("NODE_ENV") {
       if node_env == "test" {
         println!("resources names: {:?}", resources.keys());
       }
