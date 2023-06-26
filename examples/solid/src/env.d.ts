f306e13c3be1b85b549165fd3b70630a5c41a59e
wjq990112 <1163585385@qq.com>
2023-06-26 15:12:28 +0800

    feat: support SolidJS by @farmfe/js-plugin-solid

---
+++ examples/solid/src/env.d.ts
@@ -0,0 +1,16 @@
+declare const __DEV__: boolean;
+declare module '*.svg' {
+  export default string;
+}
+declare module '*.module.css' {
+  export default Record<string, string>;
+}
+declare module '*.module.sass' {
+  export default Record<string, string>;
+}
+declare module '*.module.scss' {
+  export default Record<string, string>;
+}
+declare module '*.module.less' {
+  export default Record<string, string>;
+}
