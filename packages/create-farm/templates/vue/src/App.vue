bbf4090bf586f4928e66eeb136064348ca96d333
erkelost <1256029807@qq.com>
2023-07-22 14:28:29 +0800

    chore: update css theme color

--- packages/create-farm/templates/vue/src/App.vue
+++ packages/create-farm/templates/vue/src/App.vue
@@ -22,7 +22,7 @@ import HelloWorld from './components/HelloWorld.vue'
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #9F1A8Faa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
