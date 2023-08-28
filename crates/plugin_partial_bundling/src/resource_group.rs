34cfc615cbd11c42828da38335bd27c20caaf69b
shulandmimi <sshuang141@163.com>
2023-08-28 18:32:21 +0800

    fix: common resource pot require repeat

--- crates/plugin_partial_bundling/src/resource_group.rs
+++ crates/plugin_partial_bundling/src/resource_group.rs
@@ -1,6 +1,9 @@
 use farmfe_core::{
   hashbrown::{HashMap, HashSet},
-  module::ModuleId,
+  module::{
+    module_graph::{self, ModuleGraph},
+    ModuleId,
+  },
   resource::resource_pot::{ResourcePot, ResourcePotId, ResourcePotType},
 };
 use farmfe_toolkit::hash::sha256;
@@ -25,7 +28,7 @@ where
     .join("_")
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ResourceUnitGroup {
   pub resource_unit: ResourceUnit,
   pub groups: HashSet<ResourceUnitId>,
@@ -44,17 +47,9 @@ impl ResourceUnitGroup {
   pub fn add_group(&mut self, resource_pot_id: &ResourceUnitId) {
     self.groups.insert(resource_pot_id.clone());
   }
-
-  pub fn remove_module(&mut self, module_id: &ModuleId) -> bool {
-    self.resource_unit.remove_module(module_id)
-  }
-
-  pub fn add_module(&mut self, module_id: &ModuleId) {
-    self.resource_unit.add_module(module_id.clone());
-  }
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug)]
 pub struct ResourceUnit {
   modules: HashSet<ModuleId>,
   pub resource_pot_type: Option<ResourcePotType>,
@@ -64,17 +59,21 @@ pub struct ResourceUnit {
   pub id: ResourceUnitId,
 }
 
+fn random_id() -> String {
+  rand::thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(8)
+    .map(char::from)
+    .collect::<String>()
+}
+
 pub type ResourceUnitId = String;
 
 impl ResourceUnit {
   pub fn new(name: String) -> Self {
     Self {
       name,
-      id: rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(8)
-        .map(char::from)
-        .collect::<String>(),
+      id: random_id(),
       ..Default::default()
     }
   }
@@ -97,6 +96,19 @@ impl ResourceUnit {
 
   pub fn take_modules(&mut self) -> HashSet<ModuleId> {
     self.modules.drain().collect()
+  }
+}
+
+impl Clone for ResourceUnit {
+  fn clone(&self) -> Self {
+    Self {
+      modules: HashSet::new(),
+      resource_pot_type: self.resource_pot_type.clone(),
+      immutable: self.immutable.clone(),
+      name: self.name.clone(),
+      entry_module: None,
+      id: random_id(),
+    }
   }
 }
 
@@ -132,9 +144,18 @@ impl Into<ResourcePot> for ResourceUnit {
 #[derive(Debug)]
 pub struct ResourceGroup {
   pub resource_pot_group_map: HashMap<ResourceUnitId, ResourceUnitGroup>,
+  pub module_resource_map: HashMap<ModuleId, HashSet<ResourceUnitId>>,
+  pub resource_module_map: HashMap<ResourceUnitId, HashSet<ModuleId>>,
 }
 
 impl ResourceGroup {
+  pub fn new() -> Self {
+    Self {
+      resource_pot_group_map: HashMap::new(),
+      module_resource_map: HashMap::new(),
+      resource_module_map: HashMap::new(),
+    }
+  }
   pub fn add_resource_pot(&mut self, resource_unit: ResourceUnit) {
     let resource_unit_id = resource_unit.id.clone();
 
@@ -145,6 +166,12 @@ impl ResourceGroup {
         groups: HashSet::from([resource_unit_id]),
       },
     );
+  }
+
+  pub fn add_unit_group(&mut self, resource_group: ResourceUnitGroup) {
+    self
+      .resource_pot_group_map
+      .insert(resource_group.resource_unit.id.clone(), resource_group);
   }
 
   pub fn remove_resource_pot(&mut self, resource_unit_id: &ResourceUnitId) {
@@ -166,15 +193,81 @@ impl ResourceGroup {
       .collect()
   }
 
-  pub fn to_resources(self) -> Vec<ResourcePot> {
+  pub fn to_resources(self, module_graph: &ModuleGraph) -> Vec<ResourcePot> {
+    let mut resource_pot_groups_map: HashMap<ResourceUnitId, HashSet<ModuleId>> = HashMap::new();
+
+    self.resource_pot_group_map.keys().cloned().for_each(
+      |resource_unit_group_id: ResourceUnitId| {
+        let mut group_modules = HashSet::new();
+        let mut next_id = vec![resource_unit_group_id.clone()];
+
+        while let Some(unit_group_id) = next_id.pop() {
+          if let Some(cache_groups) = resource_pot_groups_map.get(&unit_group_id) {
+            group_modules.extend(cache_groups.iter().cloned());
+            continue;
+          }
+
+          if let Some(unit_group) = self.resource_pot_group_map.get(&unit_group_id) {
+            if unit_group.resource_unit.entry_module.is_some() {
+              group_modules.insert(
+                unit_group
+                  .resource_unit
+                  .entry_module
+                  .as_ref()
+                  .unwrap()
+                  .clone(),
+              );
+            } else {
+              next_id.extend(
+                unit_group
+                  .groups
+                  .iter()
+                  .filter(|item| unit_group_id != **item)
+                  .cloned(),
+              );
+            }
+          }
+        }
+        if group_modules.is_empty() {
+          let unit_group = self
+            .resource_pot_group_map
+            .get(&resource_unit_group_id)
+            .unwrap();
+
+          unit_group
+            .resource_unit
+            .modules()
+            .iter()
+            .for_each(|module_id| {
+              let module = module_graph.module(module_id).unwrap();
+
+              group_modules.extend(module.module_groups.iter().cloned().collect::<HashSet<_>>());
+            });
+        }
+        resource_pot_groups_map.insert(resource_unit_group_id, group_modules);
+      },
+    );
+
     self
       .resource_pot_group_map
       .into_values()
-      .map(|group| group.resource_unit.into())
+      .map(|unit_group| {
+        let groups = resource_pot_groups_map
+          .remove(&unit_group.resource_unit.id)
+          .unwrap_or_default();
+
+        (unit_group, groups)
+      })
+      .map(|(group, groups)| {
+        let mut resource_pot: ResourcePot = group.resource_unit.into();
+
+        resource_pot.module_groups = groups;
+        resource_pot
+      })
       .collect()
   }
 
-  pub fn resource_pot(&self, resource_unit_id: &ResourceUnitId) -> Option<&ResourceUnit> {
+  pub fn resource_unit(&self, resource_unit_id: &ResourceUnitId) -> Option<&ResourceUnit> {
     self
       .resource_pot_group_map
       .get(resource_unit_id)
@@ -182,7 +275,7 @@ impl ResourceGroup {
       .map(|group| &group.resource_unit)
   }
 
-  pub fn resource_pot_mut(
+  pub fn resource_unit_mut(
     &mut self,
     resource_unit_id: &ResourceUnitId,
   ) -> Option<&mut ResourceUnit> {
--- crates/plugin_partial_bundling/src/split_resource_unit.rs
+++ crates/plugin_partial_bundling/src/split_resource_unit.rs
@@ -3,7 +3,7 @@ use farmfe_core::{
   module::{module_graph::ModuleGraph, ModuleId, ModuleType},
 };
 
-use crate::{ResourceGroup, ResourceUnit, ResourceUnitId};
+use crate::{ResourceGroup, ResourceUnitId};
 
 pub fn split_resource_by_module_metadata(
   resource_group: &mut ResourceGroup,
@@ -11,18 +11,17 @@ pub fn split_resource_by_module_metadata(
   module_graph: &mut ModuleGraph,
 ) {
   if resource_group
-    .resource_pot(resource_unit_id)
+    .resource_unit(resource_unit_id)
     .is_some_and(|resource_unit| resource_unit.modules().is_empty())
   {
     return;
   }
 
-  let resource_unit = resource_group.resource_pot_mut(resource_unit_id).unwrap();
-  let resource_unit_name = resource_unit.name.clone();
+  let resource_unit_group = resource_group.group_mut(resource_unit_id).unwrap();
   let mut resource_map: HashMap<(ModuleType, bool), Vec<ModuleId>> = HashMap::new();
-  let is_entry = resource_unit.entry_module.is_some();
+  let is_entry = resource_unit_group.resource_unit.entry_module.is_some();
 
-  for module_id in resource_unit.take_modules() {
+  for module_id in resource_unit_group.resource_unit.take_modules() {
     let module = module_graph.module(&module_id).unwrap();
 
     resource_map
@@ -33,36 +32,38 @@ pub fn split_resource_by_module_metadata(
 
   if resource_map.len() == 1 {
     let ((module_type, inmutable), modules) = resource_map.into_iter().next().unwrap();
-    resource_unit.replace_modules(modules.into_iter().collect());
-    resource_unit.resource_pot_type = Some(module_type.into());
-    resource_unit.immutable = inmutable;
+    resource_unit_group
+      .resource_unit
+      .replace_modules(modules.into_iter().collect());
+    resource_unit_group.resource_unit.resource_pot_type = Some(module_type.into());
+    resource_unit_group.resource_unit.immutable = inmutable;
     return;
   }
 
   let new_resource_pots = resource_map
     .into_iter()
     .map(|((module_type, immutable), modules)| {
-      let resource_unit_name = resource_unit_name.clone();
+      let mut new_resource_unit_group = resource_unit_group.clone();
 
-      let mut new_resource_unit = ResourceUnit::new(resource_unit_name);
-
-      new_resource_unit.resource_pot_type = Some(module_type.into());
-      new_resource_unit.immutable = immutable;
+      new_resource_unit_group.resource_unit.resource_pot_type = Some(module_type.into());
+      new_resource_unit_group.resource_unit.immutable = immutable;
 
       modules.into_iter().for_each(|module_id| {
         if is_entry && module_graph.entries.contains_key(&module_id) {
-          new_resource_unit.entry_module = Some(module_id.clone());
+          new_resource_unit_group.resource_unit.entry_module = Some(module_id.clone());
         }
 
-        new_resource_unit.add_module(module_id);
+        new_resource_unit_group.resource_unit.add_module(module_id);
       });
 
-      new_resource_unit
+      new_resource_unit_group
     })
     .collect::<Vec<_>>();
 
-  new_resource_pots.into_iter().for_each(|resource_unit| {
-    resource_group.add_resource_pot(resource_unit);
-  });
+  new_resource_pots
+    .into_iter()
+    .for_each(|resource_unit_group| {
+      resource_group.add_unit_group(resource_unit_group);
+    });
   resource_group.remove_resource_pot(resource_unit_id);
 }
