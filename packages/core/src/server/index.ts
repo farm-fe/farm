3580fe8eedfd2d47938f594d10bd1c7d04dd7361
Nirvana <1357711537@qq.com>
2023-09-08 03:01:31 +0800

    fix: publicPath adapted to the relative path

--- packages/core/src/server/index.ts
+++ packages/core/src/server/index.ts
@@ -13,6 +13,7 @@ import {
   NormalizedServerConfig,
   normalizeDevServerOptions,
   normalizePublicDir,
+  normalizePublicPath,
   DevServerPlugin,
   UserConfig,
   DEFAULT_HMR_OPTIONS
@@ -73,18 +74,9 @@ export class DevServer implements ImplDevServer {
       options.publicDir
     );
 
-    this.publicPath = options?.compilation?.output?.publicPath || '/';
-
-    if (
-      this.publicPath.startsWith('/') &&
-      !this.publicPath.startsWith('http')
-    ) {
-      this.publicPath = this.publicPath.slice(1);
-    }
-
-    if (!this.publicPath.endsWith('/') && !this.publicPath.startsWith('http')) {
-      this.publicPath = this.publicPath + '/';
-    }
+    this.publicPath =
+      normalizePublicPath(options?.compilation?.output?.publicPath, logger) ||
+      '/';
 
     this.userConfig = options;
     this.createFarmServer(options.server);
@@ -103,6 +95,9 @@ export class DevServer implements ImplDevServer {
       this.logger.error('HTTP server is not created yet');
     }
     const { port, open, protocol, hostname, host } = this.config;
+    const publicPath = this.publicPath.startsWith('/')
+      ? this.publicPath
+      : `/${this.publicPath}`;
     const start = Date.now();
     // compile the project and start the dev server
     if (process.env.FARM_PROFILE) {
@@ -121,7 +116,7 @@ export class DevServer implements ImplDevServer {
     this.error(port, host);
     this.startDevLogger(start, end);
     if (open) {
-      openBrowser(`${protocol}://${hostname}:${port}`);
+      openBrowser(`${protocol}://${hostname}:${port}${publicPath}`);
     }
   }
 
@@ -268,6 +263,9 @@ export class DevServer implements ImplDevServer {
 
   private startDevLogger(start: number, end: number) {
     const { port, protocol, hostname } = this.config;
+    const publicPath = this.publicPath.startsWith('/')
+      ? this.publicPath
+      : `/${this.publicPath}`;
     const version = JSON.parse(
       readFileSync(
         join(fileURLToPath(import.meta.url), '../../../package.json'),
@@ -284,7 +282,7 @@ export class DevServer implements ImplDevServer {
   Version ${chalk.green.bold(version)}
 
   🔥 Ready on ${chalk.green.bold(
-    `${protocol}://${hostname}:${port}`
+    `${protocol}://${hostname}:${port}${publicPath}`
   )} in ${chalk.green.bold(`${end - start}ms`)}.
     `,
         {
