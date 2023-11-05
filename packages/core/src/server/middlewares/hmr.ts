dc049bfd972313dbfd7ccb2f774468cbd5cd805a
erkelost <1256029807@qq.com>
2023-11-06 07:24:08 +0800

    chore: update code

--- packages/core/src/server/middlewares/hmr.ts
+++ packages/core/src/server/middlewares/hmr.ts
@@ -12,36 +12,11 @@ import { DevServer } from '../index.js';
 //  * @deprecated HMR result is now served by websocket, send to client via websocket and get the result by `eval` the websocket message.
 //  * @param context DevServer
 //  */
-// export function hmr(server: DevServer) {
-//   return async (ctx: Context, next: () => Promise<void>) => {
-//     if (ctx.path === '/__hmr') {
-//       const result = server.hmrEngine?.getUpdateResult?.(
-//         ctx.query.id as string
-//       );
-
-//       if (result) {
-//         ctx.status = 200;
-//         ctx.type = 'application/javascript';
-//         ctx.body = result;
-//       } else {
-//         throw new Error(`HMR update result not found for id ${ctx.query.id}`);
-//       }
-//     } else {
-//       await next();
-//     }
-//   };
-// }
 
 export function hmrPlugin(devSeverContext: DevServer) {
   const { config, _context, logger } = devSeverContext;
   if (config.hmr) {
-    if (!(config.hmr.host === config.host && config.hmr.port === config.port)) {
-      // devSeverContext.ws = new WebSocketServer({
-      //   port: config.hmr.port,
-      //   host: config.hmr.host as string,
-      //   path: config.hmr.path,
-      // });
-    }
+    // TODO use diff hmr port with dev server
     devSeverContext.hmrEngine = new HmrEngine(
       _context.compiler,
       devSeverContext,
