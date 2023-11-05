d9994a379e7727f808046f455badf808de6f5df0
erkelost <1256029807@qq.com>
2023-11-06 07:18:43 +0800

    chore: update code

--- packages/runtime-plugin-hmr/src/index.ts
+++ packages/runtime-plugin-hmr/src/index.ts
@@ -24,9 +24,7 @@ export default <FarmRuntimePlugin>{
 
     function connect() {
       // setup websocket connection
-      console.log(`ws://${host}:${port}${path}`);
-
-      const socket = new WebSocket(`ws://${host}:${9000}${path}`, 'farm_hmr');
+      const socket = new WebSocket(`ws://${host}:${port}${path}`, 'farm_hmr');
       // listen for the message from the server
       // when the user save the file, the server will recompile the file(and its dependencies as long as its dependencies are changed)
       // after the file is recompiled, the server will generated a update resource and send its id to the client
