ff72e0ff21927954e4a49869501e769297cab7e7
erkelost <1256029807@qq.com>
2023-11-12 20:30:47 +0800

    chore: update cli func

--- packages/cli/src/plugin/create.ts
+++ packages/cli/src/plugin/create.ts
@@ -1,47 +1,89 @@
 import path from 'node:path';
 import inquirer from 'inquirer';
-import chalk from 'chalk';
 import { copyFiles, TEMPLATES_DIR } from '../utils.js';
+import { bold, green } from '../color.js';
 
 export interface CreateArgs {
   npmName?: string;
   structName?: string;
   dir?: string;
+  language?: 'rust' | 'js';
 }
 
-const TEMPLATE_PLUGIN = path.join(TEMPLATES_DIR, 'rust-plugin');
 const TEMPLATE_NPM_NAME = '<FARM-RUST-PLUGIN-NPM-NAME>';
+const TEMPLATE_JS_NPM_NAME = '<FARM-JS-PLUGIN-NPM-NAME>';
 const TEMPLATE_STRUCT_NAME = '<FARM-RUST-PLUGIN-STRUCT-NAME>';
 
 /**
- * Farm plugin create command, create a rust farm plugin
+ * Farm plugin create command, create a rust / js farm plugin
  */
 export async function create(args: CreateArgs): Promise<void> {
   const prompts = [];
+  const commonPrompts = [
+    {
+      type: 'list',
+      name: 'language',
+      message: 'What type of plugin do you want to create?',
+      choices: [
+        {
+          name: 'Rust plugin',
+          value: 'rust'
+        },
+        {
+          name: 'Javascript plugin',
+          value: 'js'
+        }
+      ]
+    }
+  ];
 
-  if (!args.dir) {
-    prompts.push({
+  const rustPrompts = [
+    {
       type: 'input',
       name: 'dir',
       message: 'Where to create the plugin? E.g. ./farm-plugin-xxx',
-      default: '.'
-    });
-  }
-
-  if (!args.npmName) {
-    prompts.push({
+      default: './farm-plugin-xxx'
+    },
+    {
       type: 'input',
       name: 'npmName',
-      message: 'What is the npm name of the plugin? E.g. farmfe-plugin-xxx'
-    });
-  }
-
-  if (!args.structName) {
-    prompts.push({
+      message: 'What is the npm name of the plugin? E.g. farm-plugin-xxx'
+    },
+    {
       type: 'input',
       name: 'structName',
       message: 'What is the name struct of the plugin? E.g. FarmPluginXxx'
-    });
+    }
+  ];
+
+  const jsPrompts = [
+    {
+      type: 'input',
+      name: 'dir',
+      message: 'Where to create the plugin? E.g. ./farm-js-plugin-xxx',
+      default: './farm-js-plugin-xxx'
+    },
+    {
+      type: 'input',
+      name: 'npmName',
+      message: 'What is the npm name of the plugin? E.g. farm-js-plugin-xxx'
+    }
+  ];
+
+  if (!args.language) {
+    if (!args.language) {
+      const languageChoice = await inquirer.prompt(commonPrompts);
+      args.language = languageChoice.language;
+    }
+  }
+  const selectedPrompts = args.language === 'rust' ? rustPrompts : jsPrompts;
+
+  for (const prompt of selectedPrompts) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (!args[prompt.name]) {
+      prompts.push(prompt);
+    }
   }
 
   const ans = await inquirer.prompt(prompts);
@@ -50,12 +92,26 @@ export async function create(args: CreateArgs): Promise<void> {
   const structName = args.structName || ans.nameStruct;
 
   const dest = path.join(process.cwd(), dir);
-
-  copyFiles(TEMPLATE_PLUGIN, dest, (content) => {
-    return content
+  const language = args.language || ans.language;
+  const TEMPLATE_PLUGIN = new Map([
+    ['rust', path.join(TEMPLATES_DIR, 'rust-plugin')],
+    ['js', path.join(TEMPLATES_DIR, 'js-plugin')]
+  ]);
+  copyFiles(TEMPLATE_PLUGIN.get(language), dest, (content) => {
+    const rustContent = content
       .replaceAll(new RegExp(TEMPLATE_NPM_NAME, 'g'), npmName)
       .replaceAll(new RegExp(TEMPLATE_STRUCT_NAME, 'g'), structName);
+    const jsContent = content.replaceAll(
+      new RegExp(TEMPLATE_JS_NPM_NAME, 'g'),
+      npmName
+    );
+
+    const TEMPLATE_PLUGIN = new Map([
+      ['rust', rustContent],
+      ['js', jsContent]
+    ]);
+    return TEMPLATE_PLUGIN.get(language);
   });
 
-  console.log(chalk.green(`Plugin created successfully in ${dest}`));
+  console.log(bold(green(`Plugin created successfully in ${dest}`)));
 }
---
+++ packages/cli/templates/js-plugin/CHANGELOG.md
@@ -0,0 +1,25 @@
+# @farmfe/js-plugin-dts
+
+## 0.2.1
+
+### Patch Changes
+
+- ec3e6ae: support alias
+
+## 0.2.0
+
+### Minor Changes
+
+- dcce5f6: support cjs output
+
+## 0.1.2
+
+### Patch Changes
+
+- 6ab20df: Reduce production volume and remove sourcemap
+
+## 0.1.1
+
+### Patch Changes
+
+- 6379d75: feat: add dts plugins with farm
