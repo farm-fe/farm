5916972cdc0309e4ab2aed6227bdad8301b4f85c
brightwu <1521488775@qq.com>
2023-11-28 14:03:17 +0800

    chore: downgrade glibc and support win32-ia32 and win32-arm64 (#792)
    * chore: downgrade glibc and support win32-ia32 and win32-arm64
    
    * chore: remove duplicate jobs
    
    * fix: build fail
    
    * chore: remove all separate plugin build
    
    * chore: update ci
    
    * chore: setup protoc
    
    * chore: update artifacts upload
    
    * fix: window build path error
    
    * chore: cross compile macos
    
    * chore: use zig to cross compile mac on linux
    
    * chore: use native macos build
    
    ---------
    
    Co-authored-by: brightwwu <brightwwu@tencent.com>
    Co-authored-by: ADNY <66500121+ErKeLost@users.noreply.github.com>

---
+++ rust-plugins/react/npm/win32-ia32-msvc/README.md
@@ -0,0 +1,3 @@
+# `@farmfe/core-win32-ia32-msvc`
+
+This is the i686-pc-windows-msvc binary for `@farmfe/core`
--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,7 +1,11 @@
 import { execSync } from "child_process";
+import { buildCli, buildCoreCjs, buildJsPlugins } from './build.mjs';
 
 // build node packages
-execSync("npm run build", { stdio: "inherit" });
+await buildCli();
+await buildCoreCjs();
+await buildJsPlugins();
+
 execSync("npm config set access public", { stdio: "inherit" });
 // publish node packages
 execSync("npx changeset publish", { stdio: "inherit" });
