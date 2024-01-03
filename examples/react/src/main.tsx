7be21f11bdbdd575c9b25693c938a1bbfc09ba46
erkelost <1256029807@qq.com>
2024-01-03 09:22:07 +0800

    chore: update code

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Welcome } from './components/index';
 import './main.css';
 import { Button } from '@farmfe-examples/lib-for-browser';
+console.log(process);
 
 export function Main() {
   return (
--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -252,15 +252,13 @@ export async function normalizeUserCompilationConfig(
       ['FARM' + '_PROCESS_ENV']: userConfig.env
     },
     config?.define,
-
     // for node target, we should not define process.env.NODE_ENV
     config.output?.targetEnv === 'node'
       ? {}
-      : {
-          process: {
-            env: userConfig.env
-          }
-        }
+      : Object.keys(userConfig.env || {}).reduce((env: any, key) => {
+          env[`process.env.${key}`] = userConfig.env[key];
+          return env;
+        }, {})
   );
 
   await normalizePersistentCache(config, userConfig);
