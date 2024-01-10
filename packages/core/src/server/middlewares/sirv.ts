98c5c410a4ce1dc934e2f8ee8e1adb5527dcdda3
erkelost <1256029807@qq.com>
2024-01-10 17:59:40 +0800

    chore: update code

--- packages/core/src/server/middlewares/sirv.ts
+++ packages/core/src/server/middlewares/sirv.ts
@@ -1,9 +1,10 @@
-import { Middleware, Context } from 'koa';
+import { Middleware, Context, Next } from 'koa';
 import { DevServer } from '../index.js';
 import sirv from 'sirv';
 
 export function sirvMiddleware(devSeverContext: DevServer): Middleware {
   const { config } = devSeverContext._context;
+  const handleStatic = StaticFilesHandler(config.output.path);
 
   return async (ctx: Context, next) => {
     const requestPath = ctx.request?.path;
@@ -20,8 +21,12 @@ export function sirvMiddleware(devSeverContext: DevServer): Middleware {
       }
     }
 
-    const handleStatic = StaticFilesHandler(config.output.path);
-    await handleStatic(ctx, next);
+    try {
+      await handleStatic(ctx, next);
+    } catch (error) {
+      this.logger.error('Static file handling error:', error);
+      ctx.status = 500;
+    }
   };
 }
 
@@ -31,7 +36,7 @@ export function StaticFilesHandler(distDir: string) {
     single: true
   });
 
-  return async (ctx: Context, next: () => Promise<any>) => {
+  return async (ctx: Context, next: Next) => {
     await new Promise<void>((resolve) => {
       staticFilesServer(ctx.req, ctx.res, () => {
         resolve();
