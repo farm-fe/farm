5de81eaa0aab325ef2e1d4d17399bb2d83a1f201
erkelost <1256029807@qq.com>
2024-01-10 21:55:17 +0800

    chore: optimize proxy error

--- examples/react-antd/farm.config.ts
+++ examples/react-antd/farm.config.ts
@@ -1,4 +1,4 @@
-import type { UserConfig } from "@farmfe/core";
+import type { UserConfig } from '@farmfe/core';
 
 function defineConfig(config: UserConfig) {
   return config;
@@ -7,21 +7,25 @@ function defineConfig(config: UserConfig) {
 export default defineConfig({
   compilation: {
     input: {
-      index: "./index.html",
+      index: './index.html'
     },
     output: {
-      path: "./build",
-      publicPath: "/admin/",
+      path: './build',
+      publicPath: '/admin/'
     },
-    sourcemap: false,
-    persistentCache: true,
+    sourcemap: true,
+    persistentCache: true
   },
   server: {
-    // headers: {
-    //   'Access-Control-Allow-Origin': '*'
-    // },
     writeToDisk: false,
     cors: true,
+    proxy: {
+      '/api': {
+        target: 'https://music-erkelost.vercel.app/banner',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
-  plugins: ["@farmfe/plugin-react", "@farmfe/plugin-sass"],
+  plugins: ['@farmfe/plugin-react', '@farmfe/plugin-sass']
 });
--- packages/core/src/server/middlewares/resources.ts
+++ packages/core/src/server/middlewares/resources.ts
@@ -31,7 +31,7 @@ export function resourcesMiddleware(
     }
 
     // Fallback to index.html if the resource is not found
-    let resourcePath = ctx.path.slice(1) || 'index.html'; // remove leading slash
+    let resourcePath = ctx.path?.slice(1) || 'index.html'; // remove leading slash
 
     // output_files
     if (resourcePath === '_output_files') {
