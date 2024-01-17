6d1e90ef4deadb0cb9d12115037ebcf1c3abab6c
Bright Wu <1521488775@qq.com>
2024-01-17 22:29:32 +0800

    chore: add svelte example

---
+++ packages/core/src/plugin/js/constants.ts
@@ -0,0 +1,38 @@
+export const VITE_DEFAULT_ASSETS = [
+  // images
+  'apng',
+  'png',
+  'jpe?g',
+  'jfif',
+  'pjpeg',
+  'pjp',
+  'gif',
+  'svg',
+  'ico',
+  'webp',
+  'avif',
+
+  // media
+  'mp4',
+  'webm',
+  'ogg',
+  'mp3',
+  'wav',
+  'flac',
+  'aac',
+  'opus',
+  'mov',
+  'm4a',
+  'vtt',
+
+  // fonts
+  'woff2?',
+  'eot',
+  'ttf',
+  'otf',
+
+  // other
+  'webmanifest',
+  'pdf',
+  'txt'
+];
