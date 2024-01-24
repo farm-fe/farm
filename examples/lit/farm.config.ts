1e5954d6ee3c8c910ded2e4c4a58e210e1b0d527
erkelost <1256029807@qq.com>
2024-01-24 11:21:50 +0800

    chore: add lit example

---
+++ examples/lit/farm.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@farmfe/core';
+
+export default defineConfig({
+  compilation: {
+    minify: false,
+    persistentCache: false,
+    script: {
+      plugins: [],
+      target: 'es2022',
+      parser: {
+        tsConfig: {
+          decorators: true,
+          dts: false,
+          noEarlyErrors: false,
+          tsx: false,
+        },
+      },
+      decorators: {
+        legacyDecorator: true,
+        decoratorMetadata: false,
+        decoratorVersion: "2021-12",
+        includes: ["src/my-element.ts"],
+        excludes: ['node_modules/'],
+      }
+    },
+  },
+});
