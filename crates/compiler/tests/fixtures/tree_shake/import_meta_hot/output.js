ef88921988cd891a91a8836d239865dfbaff0803
shulandmimi <sshuang141@gmail.com>
2024-03-17 21:30:05 +0800

    chore: update test fixtures

--- crates/compiler/tests/fixtures/tree_shake/import_meta_hot/output.js
+++ crates/compiler/tests/fixtures/tree_shake/import_meta_hot/output.js
@@ -1,83 +1,57 @@
 //index.js:
- (globalThis || window || global)['__farm_default_namespace__'] = {__FARM_TARGET_ENV__: 'browser'};(function (modules, entryModule) {
-            var cache = {};
-
-            function dynamicRequire(id) {
-              return Promise.resolve(require(id));
-            }
-          
-            function require(id) {
-              if (cache[id]) return cache[id].exports;
-          
-              var module = {
-                id: id,
-                exports: {}
-              };
-          
-              modules[id](module, module.exports, require, dynamicRequire);
-              cache[id] = module;
-              return module.exports;
-            }
-          
-            require(entryModule);
-          })({"d2214aaa": function(module, exports, farmRequire, farmDynamicRequire) {
-console.log("runtime/index.js")(globalThis || window || global)["__farm_default_namespace__"].__farm_module_system__.setPlugins([]);
-
-},}, "d2214aaa");(function (modules) {
-            for (var key in modules) {
-              modules[key].__farm_resource_pot__ = 'index_ddf1.js';
-                (globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.register(key, modules[key]);
-            }
-        })({"05ee5ec7": function(module, exports, farmRequire, farmDynamicRequire) {
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-function _export(target, all) {
-    for(var name in all)Object.defineProperty(target, name, {
-        enumerable: true,
-        get: all[name]
+ (globalThis || window || global)['__farm_default_namespace__'] = {__FARM_TARGET_ENV__: 'browser'};(function(r,e){var t={};function n(r){return Promise.resolve(o(r))}function o(e){if(t[e])return t[e].exports;var i={id:e,exports:{}};r[e](i,i.exports,o,n);t[e]=i;return i.exports}o(e)})({"d2214aaa":function  (module, exports, farmRequire, farmDynamicRequire) {
+    console.log("runtime/index.js")(globalThis || window || global)["__farm_default_namespace__"].__farm_module_system__.setPlugins([]);
+}
+,},"d2214aaa");(function(_){for(var r in _){_[r].__farm_resource_pot__='index_ddf1.js';(globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.register(r,_[r])}})({"05ee5ec7":function  (module, exports, farmRequire, farmDynamicRequire) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", {
+        value: true
     });
-}
-_export(exports, {
-    a: function() {
-        return a;
-    },
-    invalidate: function() {
-        return invalidate;
+    function _export(target, all) {
+        for(var name in all)Object.defineProperty(target, name, {
+            enumerable: true,
+            get: all[name]
+        });
     }
-});
-const a = "1";
-function invalidate() {
-    return `invalidate data`;
-}
-
-},
-"b5d64806": function(module, exports, farmRequire, farmDynamicRequire) {
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-Object.defineProperty(exports, "InvalidateParent", {
-    enumerable: true,
-    get: function() {
-        return InvalidateParent;
-    }
-});
-var _dep = farmRequire("05ee5ec7");
-console.log(_dep.a);
-const id = "InvalidateParent";
-function InvalidateParent() {
-    return {
-        render: ()=>{
-            const renderData = (0, _dep.invalidate)();
-            const div = document.createElement("div", {});
-            div.id = id;
-            div.innerText = renderData;
-            div.className = "box";
-            return div;
+    _export(exports, {
+        a: function() {
+            return a;
+        },
+        invalidate: function() {
+            return invalidate;
         }
-    };
+    });
+    const a = "1";
+    function invalidate() {
+        return `invalidate data`;
+    }
 }
-
-},});(globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.setInitialLoadedResources([]);(globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.setDynamicModuleResourcesMap({  });var farmModuleSystem = (globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__;farmModuleSystem.bootstrap();var entry = farmModuleSystem.require("b5d64806");var InvalidateParent=entry.InvalidateParent;export { InvalidateParent };
+,
+"b5d64806":function  (module, exports, farmRequire, farmDynamicRequire) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    Object.defineProperty(exports, "InvalidateParent", {
+        enumerable: true,
+        get: function() {
+            return InvalidateParent;
+        }
+    });
+    var _dep = farmRequire("05ee5ec7");
+    console.log(_dep.a);
+    const id = "InvalidateParent";
+    function InvalidateParent() {
+        return {
+            render: ()=>{
+                const renderData = (0, _dep.invalidate)();
+                const div = document.createElement("div", {});
+                div.id = id;
+                div.innerText = renderData;
+                div.className = "box";
+                return div;
+            }
+        };
+    }
+}
+,});(globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.setInitialLoadedResources([]);(globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__.setDynamicModuleResourcesMap({  });var farmModuleSystem = (globalThis || window || global)['__farm_default_namespace__'].__farm_module_system__;farmModuleSystem.bootstrap();var entry = farmModuleSystem.require("b5d64806");var InvalidateParent=entry.InvalidateParent;export { InvalidateParent };
