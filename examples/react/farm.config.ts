182bb6f270dcaf306aa92e5e3a489b15b4cd7ddf
erkelost <1256029807@qq.com>
2024-03-19 12:33:21 +0800

    chore: update demo

--- examples/react/farm.config.ts
+++ examples/react/farm.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   compilation: {
     sourcemap: false,
     persistentCache: false,
+    presetEnv: false
   },
   plugins: [
     ['@farmfe/plugin-react', { runtime: 'automatic' }],
