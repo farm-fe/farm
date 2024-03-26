ebd3cd7770cf5e0129bc18a77e3411eb194394b5
brightwwu <brightwwu@tencent.com>
2024-03-26 22:06:17 +0800

    chore: use deepmerge to merge config

--- packages/core/src/utils/merge.ts
+++ packages/core/src/utils/merge.ts
@@ -1,63 +1,35 @@
-function isObject(value: any): boolean {
-  return value !== null && typeof value === 'object';
-}
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore ignore type check
+import isPlainObject from 'is-plain-object';
+import deepmerge from 'deepmerge';
 
-type T = Record<string, any>;
-
-function isValueSameDeep(target: any, source: any): boolean {
-  if (target === source) {
-    return true;
-  }
-
-  if (!isObject(target) || !isObject(source)) {
-    return false;
-  }
-
-  if (Object.keys(target).length !== Object.keys(source).length) {
-    return false;
-  }
-
-  for (const key in source) {
-    if (!isValueSameDeep(target[key], source[key])) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-export default function merge(target: T, ...sources: Partial<T>[]): T {
+export default function merge(
+  target: Record<string, unknown>,
+  ...sources: Record<string, unknown>[]
+): Record<string, unknown> {
   target = { ...target };
 
-  if (!isObject(target)) {
-    return target;
-  }
-
   for (const source of sources) {
-    if (!isObject(source)) {
-      continue;
-    }
+    target = deepmerge(target, source, {
+      arrayMerge: (target, source, options) => {
+        const destination = target.slice();
 
-    for (const key in source) {
-      const targetValue = target[key];
-      const sourceValue = source[key];
-
-      if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
-        // remove duplicates
-        const value = [...targetValue];
-        for (const item of sourceValue) {
-          if (value.some((v) => isValueSameDeep(v, item))) {
-            continue;
+        source.forEach((item, index) => {
+          if (typeof destination[index] === 'undefined') {
+            destination[index] = options.cloneUnlessOtherwiseSpecified(
+              item,
+              options
+            );
+          } else if (options.isMergeableObject(item)) {
+            destination[index] = deepmerge(target[index], item, options);
+          } else if (target.indexOf(item) === -1) {
+            destination.push(item);
           }
-          value.push(item);
-        }
-        target[key] = value;
-      } else if (isObject(targetValue) && isObject(sourceValue)) {
-        target[key] = merge(targetValue, sourceValue);
-      } else if (sourceValue !== undefined) {
-        target[key] = sourceValue;
-      }
-    }
+        });
+        return destination;
+      },
+      isMergeableObject: isPlainObject
+    });
   }
 
   return target;
