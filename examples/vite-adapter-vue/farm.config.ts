cf14295b69131409260814656ba0f61c7dddb568
brightwu <1521488775@qq.com>
2024-03-29 20:37:07 +0800

    Fix/adapter config (#1096)
    * fix: vite config adapter issue
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: brightwwu <brightwwu@tencent.com>

--- examples/vite-adapter-vue/farm.config.ts
+++ examples/vite-adapter-vue/farm.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import { defineConfig } from '@farmfe/core';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
@@ -43,6 +44,7 @@ export default defineConfig({
   vitePlugins: [
     VueRouter(),
     vue(),
+    vueJsx(),
     AutoImport({
       resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
       imports: [
