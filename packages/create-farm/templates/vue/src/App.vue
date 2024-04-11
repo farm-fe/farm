918947b87ca43b1eeaf6a92da361c6dbd253ebd7
Liuqh233 <528851597@qq.com>
2024-04-12 07:14:35 +0800

    fix: adjust the position of the logo in the template (#1167)
    * fix: Adjust the position of the logo in the template
    
    * chore: add changeset

--- packages/create-farm/templates/vue/src/App.vue
+++ packages/create-farm/templates/vue/src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://farmfe.org/" target="_blank">
-      <img src="/logo.png" class="logo" alt="Farm logo" />
+      <img src="./assets/logo.png" class="logo" alt="Farm logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
