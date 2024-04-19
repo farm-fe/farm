9acf182f00936cddc4a3100429a3774cfe680642
erkelost <1256029807@qq.com>
2024-04-19 11:52:29 +0800

    chore: other command with root path

--- examples/react/farm.config.js
+++ examples/react/farm.config.js
@@ -1,30 +1,31 @@
 import { defineConfig } from '@farmfe/core';
+import path from 'path';
 
 export default defineConfig(() => {
   // console.log(__dirname);
   // console.log(__filename);
   // console.log(__dirname);
-  
+
   return {
-    // root: './react',
+    root: path.resolve(process.cwd(), './html'),
     compilation: {
       sourcemap: false,
       persistentCache: true,
       presetEnv: false,
       progress: false,
       output: {
-        publicPath: '/dist/',
-      },
+        publicPath: '/dist/'
+      }
     },
     server: {
       port: 6532,
       hmr: {
-        path: "/__farm_hmr",
+        path: '/__farm_hmr'
       }
     },
     plugins: [
       ['@farmfe/plugin-react', { runtime: 'automatic' }],
-      '@farmfe/plugin-sass',
-    ],
-  }
+      '@farmfe/plugin-sass'
+    ]
+  };
 });
