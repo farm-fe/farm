35ea1ae09135125aeb5dbc2f32a4a0dec649857e
erkelost <1256029807@qq.com>
2024-04-24 11:38:50 +0800

    feat: add tauri template

---
+++ packages/create-farm/templates/tauri/preact/src/main.tsx
@@ -0,0 +1,5 @@
+import { render } from "preact";
+import App from "./App";
+import "./styles.css";
+
+render(<App />, document.getElementById("root")!);
---
+++ packages/create-farm/templates/tauri/vanilla/farm.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@farmfe/core';
+
+export default defineConfig({
+  server: {
+    port: 1420
+  }
+});
