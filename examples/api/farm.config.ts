ba76a01a97af3d510a650534f5ab02d997d8684a
erkelost <1256029807@qq.com>
2024-04-30 14:27:23 +0800

    chore: remove example

--- examples/api/farm.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@farmfe/core';
-export default defineConfig({
-  compilation: {
-    output: {
-      targetEnv: 'node'
-    },
-    external: ['@farmfe/core']
-  }
-});
--- examples/api/index.js
+++
@@ -1,6 +0,0 @@
-import { build } from '@farmfe/core'
-
-await build({ compilation: { input: { entry: 'index.ts' } } });
-
-console.log('build success');
-
