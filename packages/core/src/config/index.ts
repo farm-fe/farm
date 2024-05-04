144960c5043f211d66c5ae983db15ff00b915c2a
erkelost <1256029807@qq.com>
2024-05-04 10:27:11 +0800

    chore: update test lint-staged

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -2,7 +2,7 @@ import crypto from 'node:crypto';
 import fs from 'node:fs';
 import module from 'node:module';
 import path, { isAbsolute, join } from "node:path";
-import { pathToFileURL } from 'node:url';
+import { pathToFileURL } from "node:url";
 
 import {
   Config,
