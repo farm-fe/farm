191a2a43d2f0cdc81f4adc6617515d3c6ffa624d
erkelost <1256029807@qq.com>
2024-06-05 13:56:51 +0800

    chore: update template deps

--- crates/create-farm-rs/templates/nestjs/src/app.module.ts
+++ crates/create-farm-rs/templates/nestjs/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users.controller';
-import { AController } from './a.controller';
-import { BController } from './b.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, AController, BController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
