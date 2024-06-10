0ece5343019df9259dc62db0a69f86f34193fe30
erkelost <1256029807@qq.com>
2024-06-10 09:47:33 +0800

    chore: update templates

--- examples/react/index.js
+++ examples/react/index.js
@@ -7,7 +7,28 @@ import {
 } from "@farmfe/core";
 
 const resolvedUserConfig = await resolveConfig({
-  plugins: ["@farmfe/plugin-react"],
+  compilation: {
+    sourcemap: true,
+    persistentCache: false,
+    presetEnv: false,
+    progress: false,
+    output: {
+      publicPath: '/dist/'
+    },
+    input: {
+      index: './index.html'
+    }
+  },
+  server: {
+    port: 6532,
+    hmr: {
+      path: '/__farm_hmr'
+    }
+  },
+  plugins: [
+    '@farmfe/plugin-react',
+    '@farmfe/plugin-sass'
+  ],
   mode: 'development',
 });
 
