1a810ddbe162bf40c343683a310b918db4277bb7
erkelost <1256029807@qq.com>
2024-07-28 12:13:03 +0800

    chore: set send req res

--- examples/refactor-react/src/main.tsx
+++ examples/refactor-react/src/main.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./main.css";
-import reactLogo from "./assets/react.svg";
-import FarmLogo from "./assets/logo.png";
+// import reactLogo from "./assets/react.svg";
+// import FarmLogo from "./assets/logo.png";
 export function Main() {
   const [count, setCount] = useState(0);
 
@@ -9,10 +9,10 @@ export function Main() {
     <>
       <div>
         <a href="https://farmfe.org/" target="_blank">
-          <img src={FarmLogo} className="logo" alt="Farm logo" />
+          {/* <img src={FarmLogo} className="logo" alt="Farm logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>Farm + React</h1>
