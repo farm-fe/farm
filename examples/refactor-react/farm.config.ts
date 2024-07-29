aa6639004c0a77143095a996d96ec27329cecce1
erkelost <1256029807@qq.com>
2024-07-29 17:28:21 +0800

    chore: update core logic

--- examples/refactor-react/farm.config.ts
+++ examples/refactor-react/farm.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from '@farmfe/core';
 export default defineConfig({
   plugins: ['@farmfe/plugin-react'],
   compilation: {
-    presetEnv: false,
-    progress: false,
-    sourcemap: false,
+    // presetEnv: false,
+    // progress: false,
+    // sourcemap: false,
+    persistentCache: false,
     runtime: {
       isolate: true
     }
