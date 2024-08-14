e60c3e9b472cd875e765a4c066c8ebafc2fede2f
erkelost <1256029807@qq.com>
2024-08-14 10:05:27 +0800

    chore: remove unless code

--- examples/refactor-react/farm.config.ts
+++ examples/refactor-react/farm.config.ts
@@ -3,20 +3,8 @@ import { defineConfig } from '@farmfe/core';
 export default defineConfig({
   plugins: ['@farmfe/plugin-react'],
   compilation: {
-    presetEnv: false,
-    minify: false,
-    progress: false,
-    sourcemap: false,
-    // persistentCache: false,
-    runtime: {
-      isolate: true
-    },
     output: {
       publicPath: '/aaa/'
     }
   },
-  server: {
-    port: 3212,
-    // writeToDisk: true,
-  }
 });
