a951b7f509e6747184509ca46d6cbfb4b67fc382
erkelost <1256029807@qq.com>
2024-09-05 17:56:52 +0800

    chore: optimize server code

--- examples/vue3/src/App.vue
+++ examples/vue3/src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 console.log(import.meta.env);
+fetch("https://corsproxy.io/?https://wallhaven.cc/api/v1/w/yx6e9l")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
 </script>
 
 <template>
