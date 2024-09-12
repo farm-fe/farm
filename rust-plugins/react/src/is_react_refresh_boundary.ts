d1f6eb81762ed59edafae52aca9113c1493ad12b
shulandmimi <sshuang141@163.com>
2024-09-12 18:50:08 +0800

    fix: class component cannot hot update

--- rust-plugins/react/src/is_react_refresh_boundary.ts
+++ rust-plugins/react/src/is_react_refresh_boundary.ts
@@ -1,5 +1,19 @@
-export default function isReactRefreshBoundary(Refresh, moduleExports): boolean {
+// @ts-ignore
+import React from 'react';
+
+function isClassComponent(v: any): boolean {
+// class component can not be hot-updated, so skip it and find its parent
+  if(v && v.prototype instanceof React.Component) {
+    return true;
+  }
+
+  return false;
+}
+
+export default function isReactRefreshBoundary(Refresh: any, moduleExports: any): boolean {
+
   if (Refresh.isLikelyComponentType(moduleExports)) {
+    if(isClassComponent(moduleExports)) return false;
     return true;
   }
 
@@ -16,9 +30,12 @@ export default function isReactRefreshBoundary(Refresh, moduleExports): boolean 
     if (key === '__esModule') {
       continue;
     }
-    
+
     const exportValue = moduleExports[key];
-    if (!Refresh.isLikelyComponentType(exportValue)) {
+
+    if(Refresh.isLikelyComponentType(exportValue)) {
+      if(isClassComponent(exportValue)) return false;
+    }else {
       areAllExportsComponents = false;
     }
   }
