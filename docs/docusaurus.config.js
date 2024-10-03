b6c08dc01882e1538dd8dcbde97d41d7108f4009
erkelost <1256029807@qq.com>
2024-10-03 21:39:20 +0800

    chore: update docs deps

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           lastVersion: "current",
           versions: {
             current: {
-              label: "2.0.0",
+              label: "2.0.0-preview",
             },
           },
         },
