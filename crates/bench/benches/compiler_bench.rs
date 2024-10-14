3ff6758e2ee36c4cc96ad487ee7b465aafc75739
erkelost <1256029807@qq.com>
2024-10-14 09:33:51 +0800

    chore: skip some tasks

--- crates/bench/benches/compiler_bench.rs
+++ crates/bench/benches/compiler_bench.rs
@@ -1,56 +1,56 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-// use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+// use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// // use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
 
-use farmfe_compiler::Compiler;
-use farmfe_core::{
-  config::{Config, RuntimeConfig},
-  relative_path::RelativePath,
-};
+// use farmfe_compiler::Compiler;
+// use farmfe_core::{
+//   config::{Config, RuntimeConfig},
+//   relative_path::RelativePath,
+// };
 
-fn setup_compiler() -> Compiler {
-  let relative_root = RelativePath::new("./index.ts");
-  let cwd = std::env::current_dir().unwrap();
-  let react_examples_root = relative_root.to_logical_path(cwd.clone());
+// fn setup_compiler() -> Compiler {
+//   let relative_root = RelativePath::new("./index.ts");
+//   let cwd = std::env::current_dir().unwrap();
+//   let react_examples_root = relative_root.to_logical_path(cwd.clone());
 
-  Compiler::new(
-    Config {
-      root: react_examples_root.to_string_lossy().to_string(),
-      runtime: Box::new(RuntimeConfig {
-        path: cwd
-          .join("packages")
-          .join("runtime")
-          .join("src")
-          .join("index.ts")
-          .to_string_lossy()
-          .to_string(),
-        plugins: vec![],
-        swc_helpers_path: cwd
-          .join("packages")
-          .join("core")
-          .join("node_modules")
-          .join("@swc")
-          .join("helpers")
-          .read_link()
-          .unwrap()
-          .to_string_lossy()
-          .to_string(),
-        ..Default::default()
-      }),
-      ..Default::default()
-    },
-    vec![],
-  )
-  .unwrap()
-}
+//   Compiler::new(
+//     Config {
+//       root: react_examples_root.to_string_lossy().to_string(),
+//       runtime: Box::new(RuntimeConfig {
+//         path: cwd
+//           .join("packages")
+//           .join("runtime")
+//           .join("src")
+//           .join("index.ts")
+//           .to_string_lossy()
+//           .to_string(),
+//         plugins: vec![],
+//         swc_helpers_path: cwd
+//           .join("packages")
+//           .join("core")
+//           .join("node_modules")
+//           .join("@swc")
+//           .join("helpers")
+//           .read_link()
+//           .unwrap()
+//           .to_string_lossy()
+//           .to_string(),
+//         ..Default::default()
+//       }),
+//       ..Default::default()
+//     },
+//     vec![],
+//   )
+//   .unwrap()
+// }
 
-fn bench_compiler_compile(c: &mut Criterion) {
-  let mut compiler = setup_compiler();
-  c.bench_function("compiler_compile", |b| {
-      b.iter(|| {
-          black_box(compiler.compile().unwrap());
-      })
-  });
-}
+// fn bench_compiler_compile(c: &mut Criterion) {
+//   let mut compiler = setup_compiler();
+//   c.bench_function("compiler_compile", |b| {
+//       b.iter(|| {
+//           black_box(compiler.compile().unwrap());
+//       })
+//   });
+// }
 
-criterion_group!(benches, bench_compiler_compile);
-criterion_main!(benches);
+// criterion_group!(benches, bench_compiler_compile);
+// criterion_main!(benches);
