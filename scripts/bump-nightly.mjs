96f9844acec6182a3b13de08b8580ac654e6eebf
erkelost <1256029807@qq.com>
2024-10-18 16:02:37 +0800

    chore: update ci

--- scripts/bump-nightly.mjs
+++ scripts/bump-nightly.mjs
@@ -5,9 +5,14 @@ const dateString =
   new Date().getFullYear() +
   String(new Date().getMonth() + 1).padStart(2, "0") +
   String(new Date().getDate()).padStart(2, "0");
-const nightlyVersion = `2.0.0-nightly.${dateString}`;
 
-execSync(`npx changeset version --snapshot ${nightlyVersion}`, { stdio: "inherit" });
+const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
+const nightlyVersion = `2.0.0-nightly.${dateString}.${gitHash}`;
+
+execSync(`npx changeset version --snapshot ${nightlyVersion}`, {
+  stdio: "inherit",
+});
 
 execSync("pnpm install --no-frozen-lockfile", { stdio: "inherit" });
 
--- scripts/release-nightly.mjs
+++ scripts/release-nightly.mjs
@@ -6,7 +6,10 @@ const dateString =
   new Date().getFullYear() +
   String(new Date().getMonth() + 1).padStart(2, "0") +
   String(new Date().getDate()).padStart(2, "0");
-const nightlyVersion = `2.0.0-nightly.${dateString}`;
+
+const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
+const nightlyVersion = `2.0.0-nightly.${dateString}.${gitHash}`;
 
 // Build node packages
 await buildCli();
@@ -17,7 +20,9 @@ await buildJsPlugins();
 execSync("npm config set access public", { stdio: "inherit" });
 
 // Update versions to nightly
-execSync(`npx changeset version --snapshot ${nightlyVersion}`, { stdio: "inherit" });
+execSync(`npx changeset version --snapshot ${nightlyVersion}`, {
+  stdio: "inherit",
+});
 
 // Publish nightly packages
 execSync(`npx changeset publish --tag ${nightlyVersion}`, { stdio: "inherit" });
