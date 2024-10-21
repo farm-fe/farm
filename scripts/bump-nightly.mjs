dcfb1e5278cbdf49d4ced714d04d1f4600ce407e
erkelost <1256029807@qq.com>
2024-10-21 16:33:55 +0800

    chore: update v2 changeset base version

--- scripts/bump-nightly.mjs
+++ scripts/bump-nightly.mjs
@@ -1,16 +1,6 @@
 import { execSync } from "child_process";
 
-// generate nightly version
-const dateString =
-  new Date().getFullYear() +
-  String(new Date().getMonth() + 1).padStart(2, "0") +
-  String(new Date().getDate()).padStart(2, "0");
-
-const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
-
-const nightlyVersion = `2.0.0-nightly.${dateString}.${gitHash}`;
-
-execSync(`npx changeset version --snapshot ${nightlyVersion}`, {
+execSync(`npx changeset version --snapshot nightly`, {
   stdio: "inherit",
 });
 
--- scripts/release-nightly.mjs
+++ scripts/release-nightly.mjs
@@ -1,16 +1,6 @@
 import { execSync } from "child_process";
 import { buildCli, buildCoreCjs, buildJsPlugins } from "./build.mjs";
 
-// Generate nightly version number
-const dateString =
-  new Date().getFullYear() +
-  String(new Date().getMonth() + 1).padStart(2, "0") +
-  String(new Date().getDate()).padStart(2, "0");
-
-const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
-
-const nightlyVersion = `2.0.0-nightly.${dateString}.${gitHash}`;
-
 // Build node packages
 await buildCli();
 await buildCoreCjs();
