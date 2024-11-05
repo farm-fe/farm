79d78f401d74e5b878f0fe434dccaf4b4e661971
shulandmimi <sshuang141@163.com>
2024-11-05 16:00:39 +0800

    fix: react refresh when concatenateModules enable

--- rust-plugins/react/src/react_refresh.rs
+++ rust-plugins/react/src/react_refresh.rs
@@ -31,11 +31,47 @@ lazy_static! {
     )
   };
 }
+// polyfill like https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/src/refreshUtils.js
 
+
+/**
+ *
+ * ```js
+ * Object.defineProperty(exports, "Foo", {
+ *  get() {
+ *    return Foo
+ *  }
+ * });
+ *
+ * Object.defineProperty(exports, "Bar", {
+ *  get() {
+ *    return Bar
+ *  }
+ * });
+ *
+ *
+ *
+ * // moduleA
+ * const Foo = () => {}
+ *
+ * // ...
+ * ReactRefreshUtil.registerExportsForReactRefresh(module.exports, module.id);
+ * // Until here, `Bar` is still not initialized
+ * // ...
+ *
+ * // moduleB
+ * const Bar = () => {}
+ * ```
+*/
+
+// When merging multiple modules, the exports will be promoted and contain side effects
+// so the getter cannot be triggered immediately when `registerExportsForReactRefresh`.
+// maybe there are other better ways.
 const POST_CODE: &str = r#"
 window.$RefreshReg$ = prevRefreshReg;
 window.$RefreshSig$ = prevRefreshSig;
 if (import.meta.hot) {
+  setTimeout(() => {
     ReactRefreshUtil.registerExportsForReactRefresh(module.exports, module.id);
 
     import.meta.hot.accept(nextExport => {
@@ -47,6 +83,7 @@ if (import.meta.hot) {
     });
 
     ReactRefreshUtil.enqueueUpdate();
+  })
 }
 "#;
 
