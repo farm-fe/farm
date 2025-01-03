35afc4bedf439380f78c5e11c4248a44b80586d1
erkelost <1256029807@qq.com>
2025-01-03 17:01:53 +0800

    chore: optimize code

--- packages/core/src/plugin/js/utils.ts
+++ packages/core/src/plugin/js/utils.ts
@@ -13,12 +13,16 @@ import type {
 
 import { VITE_ADAPTER_VIRTUAL_MODULE } from './constants.js';
 
+import { readFile } from 'node:fs/promises';
+import { ModuleContext, ModuleNode } from '../../config/types.js';
 import type { Config } from '../../types/binding.js';
 import type {
+  JsPlugin,
   JsResourcePotInfoData,
   Resource,
   ResourcePotInfo
 } from '../type.js';
+import { createModuleGraph } from './vite-server-adapter.js';
 
 export type WatchChangeEvents = 'create' | 'update' | 'delete';
 
@@ -479,6 +483,44 @@ export function normalizeFilterPath(path: string): string {
   return path;
 }
 
-function compatibleWin32Path(path: string): string {
+export function compatibleWin32Path(path: string): string {
   return path.replaceAll('/', '\\\\');
+}
+
+export function wrapPluginUpdateModules(plugin: JsPlugin): JsPlugin {
+  if (!plugin.updateModules?.executor) {
+    return plugin;
+  }
+  const originalExecutor = plugin.updateModules.executor;
+  const moduleGraph = createModuleGraph(plugin.name);
+
+  plugin.updateModules.executor = async ({ paths }, ctx) => {
+    moduleGraph.context = ctx;
+    for (const [file, type] of paths) {
+      const mods = moduleGraph.getModulesByFile(
+        file
+      ) as unknown as ModuleNode[];
+
+      const filename = normalizePath(file);
+      const moduleContext: ModuleContext = {
+        file: filename,
+        timestamp: Date.now(),
+        type,
+        paths,
+        modules: (mods ?? []).map(
+          (m) =>
+            ({
+              ...m,
+              id: normalizePath(m.id),
+              file: normalizePath(m.file)
+            }) as ModuleNode
+        ),
+        read: function (): string | Promise<string> {
+          return readFile(file, 'utf-8');
+        }
+      };
+      return originalExecutor.call(plugin, moduleContext);
+    }
+  };
+  return plugin;
 }
