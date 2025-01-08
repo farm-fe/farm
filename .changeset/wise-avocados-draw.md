c0d30091a4e6b050d802f79435ca0a6742a649c5
shulandmimi <sshuang141@163.com>
2025-01-08 21:09:24 +0800

    fix: import meta glob path failed

---
+++ .changeset/wise-avocados-draw.md
@@ -0,0 +1,5 @@
+---
+"@farmfe/core": patch
+---
+
+fix import meta glob path
--- crates/swc_transformer_import_glob/tests/mod.rs
+++ crates/swc_transformer_import_glob/tests/mod.rs
@@ -1,12 +1,113 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, path::Path, sync::Arc};
 
 use farmfe_core::{
-  context::CompilationContext, module::ModuleId, swc_common::SourceMap, swc_ecma_ast::EsVersion,
+  context::CompilationContext,
+  module::{ModuleId, VIRTUAL_MODULE_PREFIX},
+  swc_common::SourceMap,
+  swc_ecma_ast::EsVersion,
   swc_ecma_parser::Syntax,
 };
-use farmfe_swc_transformer_import_glob::transform_import_meta_glob;
+use farmfe_swc_transformer_import_glob::{
+  transform_import_meta_glob, ImportGlobVisitor, ImportMetaGlobResolver,
+  ImportMetaGlobResolverParams,
+};
 use farmfe_testing_helpers::fixture;
 use farmfe_toolkit::script::{codegen_module, parse_module, ParseScriptModuleResult};
+
+use farmfe_core::plugin::{PluginResolveHookParam, ResolveKind};
+
+struct ImportMetaGlobResolverImpl {
+  context: Arc<CompilationContext>,
+}
+
+impl ImportMetaGlobResolver for ImportMetaGlobResolverImpl {
+  fn resolve(&self, params: ImportMetaGlobResolverParams) -> Option<String> {
+    self
+      .context
+      .plugin_driver
+      .resolve(
+        &PluginResolveHookParam {
+          source: params.source,
+          importer: Some(params.importer),
+          kind: ResolveKind::Import,
+        },
+        &self.context,
+        &Default::default(),
+      )
+      .ok()
+      .flatten()
+      .map(|v| v.resolved_path)
+  }
+}
+
+fn create_context() -> Arc<CompilationContext> {
+  let mut compilation = CompilationContext::default();
+  compilation.config.root = if cfg!(windows) {
+    "C:\\root1".to_string()
+  } else {
+    "/root1/root2".to_string()
+  };
+  Arc::new(compilation)
+}
+
+fn cur_dir_by_importer<'a>(importer: &'a ModuleId, context: &'a Arc<CompilationContext>) -> String {
+  let resolved_path = importer.resolved_path(&context.config.root);
+
+  if resolved_path.starts_with(VIRTUAL_MODULE_PREFIX) {
+    context.config.root.clone()
+  } else {
+    Path::new(&resolved_path)
+      .parent()
+      .unwrap()
+      .to_string_lossy()
+      .to_string()
+  }
+}
+
+#[inline]
+fn create_visitor<'a>(
+  importer: &'a ModuleId,
+  context: &'a Arc<CompilationContext>,
+) -> ImportGlobVisitor<'a, ImportMetaGlobResolverImpl> {
+  let cur_dir = cur_dir_by_importer(importer, context);
+  let visitor = ImportGlobVisitor::new(
+    importer,
+    context.config.root.clone(),
+    cur_dir,
+    &context.config.resolve.alias,
+    ImportMetaGlobResolverImpl {
+      context: Arc::clone(context),
+    },
+  );
+
+  visitor
+}
+
+#[test]
+fn find_rel_source_absolute_path() {
+  let context = create_context();
+  let importer = ModuleId::new("src/index.js", "", &context.config.root);
+  let visitor = create_visitor(&importer, &context);
+
+  let (_, s1) = visitor.find_rel_source("/src/foo.js");
+
+  assert_eq!(s1, "src/foo.js");
+}
+
+#[test]
+fn find_rel_source_relative_path() {
+  let context = create_context();
+  let importer = ModuleId::new("src/components/welcome/index.tsx", "", &context.config.root);
+  let visitor = create_visitor(&importer, &context);
+
+  let (_, s1) = visitor.find_rel_source("../../../assets/*.js");
+
+  assert_eq!(s1, format!("assets/*.js"));
+
+  let (_, s1) = visitor.find_rel_source("../../../src/foo.js");
+
+  assert_eq!(s1, "src/foo.js");
+}
 
 #[test]
 fn test_import_meta_glob() {
@@ -48,7 +149,17 @@ fn test_import_meta_glob() {
 
     let context = Arc::new(context);
 
-    transform_import_meta_glob(&mut ast, root.to_string(), &importer, &context).unwrap();
+    transform_import_meta_glob(
+      &mut ast,
+      root.to_string(),
+      &importer,
+      cur_dir_by_importer(&importer, &context),
+      &context.config.resolve.alias,
+      ImportMetaGlobResolverImpl {
+        context: context.clone(),
+      },
+    )
+    .unwrap();
 
     let code = codegen_module(&ast, EsVersion::EsNext, cm, None, false, None).unwrap();
     let code = String::from_utf8(code).unwrap();
