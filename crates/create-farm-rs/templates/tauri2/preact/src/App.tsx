d575d7276a2c7f56450fde26a950a47365e61727
苏向夜 <46275354+fu050409@users.noreply.github.com>
2025-01-09 17:32:37 +0800

    feat(create): add support for tauri 2 (#2001)
    * rebase: pick changes to main
    
    * chore: bump version

---
+++ crates/create-farm-rs/templates/tauri2/preact/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main class="container">
+      <h1>Welcome to Tauri + Preact</h1>
+
+      <div class="row">
+        <a href="https://farmfe.org" target="_blank">
+          <img src="/farm.png" class="logo farm" alt="Farm logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <p>Click on the Tauri, Farm, and Preact logos to learn more.</p>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onInput={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
