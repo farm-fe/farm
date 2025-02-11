8dfb3d8efc7cccad9dc44915c9b4ccdd9476e445
erkelost <1256029807@qq.com>
2025-02-11 14:27:29 +0800

    chore: update biome

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -96,13 +96,13 @@ export const buildExamples = async () => {
 export async function runTaskQueue() {
   // The sass plug-in uses protobuf, so you need to determine whether the user installs it or not.
   await installProtoBuf();
-  await runTask('Cli', buildCli);
-  await runTask('Runtime', buildRuntime);
-  await runTask('PluginTools', buildPluginTools);
-  await runTask('Core', buildCore);
+  await runTask("Cli", buildCli);
+  await runTask("Runtime", buildRuntime);
+  await runTask("PluginTools", buildPluginTools);
+  await runTask("Core", buildCore);
   await runTask("RustPlugins", buildRustPlugins);
-  await runTask('JsPlugins', buildJsPlugins);
-  await runTask('Artifacts', copyArtifacts);
+  await runTask("JsPlugins", buildJsPlugins);
+  await runTask("Artifacts", copyArtifacts);
 }
 
 // install mac protobuf
