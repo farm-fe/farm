b6fc752d696fdbf9393d4e8cd310a6b88dc6e5c5
苏向夜 <fu050409@163.com>
2025-02-27 09:48:21 +0800

    feat(create-farm): integrate farm package version

--- crates/create-farm-rs/src/tasks/render.rs
+++ crates/create-farm-rs/src/tasks/render.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+use include_json::include_json;
+
 pub struct RenderTask;
 
 impl RenderTask {
@@ -13,7 +15,20 @@ impl super::Task for RenderTask {
     let target_dir: PathBuf = serde_json::from_value(ctx.get("target_dir").unwrap().clone())?;
     let template = *ctx.template();
 
+    let farm_core_pkg_json = include_json!(concat!(
+      env!("CARGO_MANIFEST_DIR"),
+      "/../../packages/core/package.json"
+    ));
+    let farm_cli_pkg_json = include_json!(concat!(
+      env!("CARGO_MANIFEST_DIR"),
+      "/../../packages/cli/package.json"
+    ));
+
+    ctx.insert("farm_core_version", farm_core_pkg_json.get("version"));
+    ctx.insert("farm_cli_version", farm_cli_pkg_json.get("version"));
+
     template.render(&target_dir, ctx)?;
+
     Ok(())
   }
 
