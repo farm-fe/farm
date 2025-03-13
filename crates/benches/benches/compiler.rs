7e73602e0e268d9ce029dd88ead5cedb87b6e219
苏向夜 <fu050409@163.com>
2025-03-13 18:31:52 +0800

    chore: ignore build errors

--- crates/benches/benches/compiler.rs
+++ crates/benches/benches/compiler.rs
@@ -2,28 +2,37 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use farmfe_compiler::Compiler;
 use farmfe_core::{
-  config::{Config, RuntimeConfig},
-  relative_path::RelativePath,
+  config::{preset_env::PresetEnvConfig, Config, RuntimeConfig},
+  HashMap,
 };
 
 fn setup_compiler() -> Compiler {
-  let relative_root = RelativePath::new("./index.ts");
   let cwd = std::env::current_dir().unwrap();
-  let react_examples_root = relative_root.to_logical_path(cwd.clone());
+  let workspace_root = cwd.parent().unwrap().parent().unwrap();
+  let example_root = workspace_root.join("examples").join("css-url");
+
+  let rt_path = workspace_root
+    .join("packages")
+    .join("runtime")
+    .join("src")
+    .join("index.ts");
+  assert!(rt_path.exists());
+
+  let input = HashMap::from_iter(vec![(
+    "index".to_string(),
+    example_root
+      .join("index.html")
+      .to_string_lossy()
+      .to_string(),
+  )]);
 
   Compiler::new(
     Config {
-      root: react_examples_root.to_string_lossy().to_string(),
+      root: example_root.to_string_lossy().to_string(),
       runtime: Box::new(RuntimeConfig {
-        path: cwd
-          .join("packages")
-          .join("runtime")
-          .join("src")
-          .join("index.ts")
-          .to_string_lossy()
-          .to_string(),
-        plugins: vec![],
-        swc_helpers_path: cwd
+        path: rt_path.to_string_lossy().to_string(),
+        plugins: vec![String::from("@farmfe/plugin-react")],
+        swc_helpers_path: workspace_root
           .join("packages")
           .join("core")
           .join("node_modules")
@@ -35,6 +44,8 @@ fn setup_compiler() -> Compiler {
           .to_string(),
         ..Default::default()
       }),
+      preset_env: Box::new(PresetEnvConfig::Bool(false)),
+      input,
       ..Default::default()
     },
     vec![],
@@ -46,7 +57,9 @@ fn bench_compiler_compile(c: &mut Criterion) {
   let compiler = setup_compiler();
   c.bench_function("compiler_compile", |b| {
     b.iter(|| {
-      black_box(compiler.compile().unwrap());
+      // black_box(compiler.compile().unwrap());
+      let compiler = setup_compiler();
+      black_box(compiler.compile())
     })
   });
 }
