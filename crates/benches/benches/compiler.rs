a1c9d17cc5092e8f464c518204268ac2475aa9cf
苏向夜 <fu050409@163.com>
2025-03-16 12:10:14 +0800

    chore: setup vanilla benchmark

--- crates/benches/benches/compiler.rs
+++ crates/benches/benches/compiler.rs
@@ -1,65 +1,39 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+use farmfe_benches::get_runtime_config;
 use farmfe_compiler::Compiler;
 use farmfe_core::{
-  config::{preset_env::PresetEnvConfig, Config, RuntimeConfig},
+  config::{persistent_cache::PersistentCacheConfig, preset_env::PresetEnvConfig, Config},
   HashMap,
 };
 
-fn setup_compiler() -> Compiler {
-  let cwd = std::env::current_dir().unwrap();
-  let workspace_root = cwd.parent().unwrap().parent().unwrap();
-  let example_root = workspace_root.join("examples").join("css-url");
-
-  let rt_path = workspace_root
-    .join("packages")
-    .join("runtime")
-    .join("src")
-    .join("index.ts");
-  assert!(rt_path.exists());
-
-  let input = HashMap::from_iter(vec![(
-    "index".to_string(),
-    example_root
-      .join("index.html")
-      .to_string_lossy()
-      .to_string(),
-  )]);
-
-  Compiler::new(
-    Config {
-      root: example_root.to_string_lossy().to_string(),
-      runtime: Box::new(RuntimeConfig {
-        path: rt_path.to_string_lossy().to_string(),
-        plugins: vec![String::from("@farmfe/plugin-react")],
-        swc_helpers_path: workspace_root
-          .join("packages")
-          .join("core")
-          .join("node_modules")
-          .join("@swc")
-          .join("helpers")
-          .to_string_lossy()
-          .to_string(),
-        ..Default::default()
-      }),
-      preset_env: Box::new(PresetEnvConfig::Bool(false)),
-      input,
-      ..Default::default()
-    },
-    vec![],
-  )
-  .unwrap()
-}
-
-fn bench_compiler_compile(c: &mut Criterion) {
-  c.bench_function("compiler_compile", |b| {
+fn compiler_compile(c: &mut Criterion) {
+  c.bench_function("rust compiler compile vanilla", |b| {
     b.iter(|| {
-      // black_box(compiler.compile().unwrap());
-      let compiler = setup_compiler();
-      black_box(compiler.compile())
-    })
+      let cwd = std::env::current_dir().unwrap();
+      let root = cwd.join("benches").join("fixtures").join("vanilla");
+
+      let compiler = Compiler::new(
+        Config {
+          root: root.to_string_lossy().to_string(),
+          input: HashMap::from_iter([(
+            "index".to_string(),
+            root.join("./index.html").to_string_lossy().to_string(),
+          )]),
+          progress: false,
+          runtime: get_runtime_config(&cwd),
+          preset_env: Box::new(PresetEnvConfig::Bool(false)),
+          persistent_cache: Box::new(PersistentCacheConfig::Bool(false)),
+          ..Default::default()
+        },
+        vec![],
+      )
+      .unwrap();
+
+      black_box(compiler.compile().unwrap());
+    });
   });
 }
 
-criterion_group!(benches, bench_compiler_compile);
+criterion_group!(benches, compiler_compile);
 criterion_main!(benches);
