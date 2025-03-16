ce56938ec88525956b1f4b13f4475446c52cfc8d
苏向夜 <fu050409@163.com>
2025-03-16 20:27:21 +0800

    chore: optimize benchmark

--- bench/config.bench.ts
+++ bench/config.bench.ts
@@ -4,14 +4,16 @@ import { getExampleRoot } from './utils';
 
 describe('resolve config', () => {
   describe('react', () => {
+    const root = getExampleRoot('react');
     bench('resolve react example config', async () => {
-      await resolveConfig({ root: getExampleRoot('react') }, 'build');
+      await resolveConfig({ root }, 'build');
     });
   });
 
   describe('vue3', () => {
+    const root = getExampleRoot('vue3');
     bench('resolve vue3 example config', async () => {
-      await resolveConfig({ root: getExampleRoot('vue3') }, 'build');
+      await resolveConfig({ root }, 'build');
     });
   });
 });
