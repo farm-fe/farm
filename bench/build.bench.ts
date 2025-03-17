6c38ba5bb479285fe8b9b186140a2930f09a89db
苏向夜 <fu050409@163.com>
2025-03-17 11:12:46 +0800

    perf(node): optimize benchmarks

--- bench/build.bench.ts
+++ bench/build.bench.ts
@@ -4,14 +4,16 @@ import { getExampleRoot } from './utils';
 
 describe('build example', { sequential: true }, () => {
   describe('react', () => {
+    const root = getExampleRoot('react');
     bench('build react example', async () => {
-      await build({ root: getExampleRoot('react') });
+      await build({ root });
     });
   });
 
   describe('vue3', () => {
+    const root = getExampleRoot('vue3');
     bench('build vue3 example', async () => {
-      await build({ root: getExampleRoot('vue3') });
+      await build({ root });
     });
   });
 });
