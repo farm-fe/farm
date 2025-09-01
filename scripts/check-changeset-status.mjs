a6d87844161fe0b8d3405d5e355fb32182f28bc9
wumingliang <wumingliang.9801@bytedance.com>
2025-09-01 19:32:37 +0800

    fix: unexpected process exit when hmr failed with syntax error & disable sourcemap in production by default

--- scripts/check-changeset-status.mjs
+++ scripts/check-changeset-status.mjs
@@ -1,8 +1,18 @@
-import { readdirSync } from "node:fs";
+import path from "node:path";
+import { existsSync, readdirSync, readFileSync } from "node:fs";
 
 const CHANGESET_DIR = "./.changeset";
 
-const changesetFilesExist = readdirSync(CHANGESET_DIR).filter((file) => file.endsWith(".md") && file !== 'README.md');
+let changesetFilesExist = readdirSync(CHANGESET_DIR).filter((file) => file.endsWith(".md") && file !== 'README.md').map(file => file.replace(".md", ""));
+
+// check pre release
+const prePath = path.join(CHANGESET_DIR, "pre.json");
+
+if (existsSync(prePath)) {
+  const preRelease = JSON.parse(readFileSync(path.join(CHANGESET_DIR, "pre.json"), "utf-8"));
+  changesetFilesExist = changesetFilesExist.filter(file => !preRelease.changesets.includes(file));
+}
+
 
 if (changesetFilesExist.length === 0) {
   console.log("No changeset files found, try publish");
