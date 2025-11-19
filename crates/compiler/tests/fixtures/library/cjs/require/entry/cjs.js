687c230a37ccaca486b5c1e9be76c491ec26be43
wumingliang <wumingliang.9801@bytedance.com>
2025-11-19 15:13:17 +0800

    fix: cjs require doesn't be handled correctly when targetEnv is library

---
+++ crates/compiler/tests/fixtures/library/cjs/require/entry/cjs.js
@@ -0,0 +1,5 @@
+module.exports.name = 123;
+module.exports.test = () => {
+  return 456;
+};
+module.exports.value = "hello";
---
+++ crates/compiler/tests/fixtures/library/cjs/require/entry/output.js
@@ -0,0 +1,51 @@
+//index.js:
+ ; // module_id: @farmfe/runtime/src/module-system.ts.farm-runtime
+// all modules registered
+const __farm_internal_modules__ = {};
+// module cache after module initialized
+const __farm_internal_cache__ = {};
+function farmRequire$1(id) {
+    if (__farm_internal_cache__[id]) {
+        var cachedModuleResult = __farm_internal_cache__[id].exports;
+        return cachedModuleResult;
+    }
+    const initializer = __farm_internal_modules__[id];
+    if (!initializer) {
+        console.debug(`[Farm] Module "${id}" is not registered`);
+        // return a empty module if the module is not registered
+        return {};
+    }
+    // create a full new module instance and store it in cache to avoid cyclic initializing
+    const module = __farm_internal_cache__[id] = {
+        id,
+        meta: {
+            env: {}
+        },
+        exports: {},
+        require: farmRequire$1
+    };
+    __farm_internal_cache__[id] = module;
+    initializer(module, module.exports);
+    // return the exports of the module
+    return module.exports;
+}
+function farmRegister(id, module) {
+    __farm_internal_modules__[id] = module;
+    return ()=>farmRequire$1(id);
+}
+; // module_id: cjs.js
+var farmRequire$2 = farmRegister("cjs.js", function(module, exports) {
+    module.exports.name = 123;
+    module.exports.test = ()=>{
+        return 456;
+    };
+    module.exports.value = "hello";
+});
+; // module_id: index.ts
+var farmRequire = farmRegister("index.ts", function(module, exports) {
+    console.log(farmRequire$2().name);
+    console.log(farmRequire$2().test);
+    console.log(farmRequire$2().test);
+});
+var index_ts_default = farmRequire();
+export { index_ts_default as default };
